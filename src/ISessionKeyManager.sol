5c36307063f3f761361d9afa1407a2e91b6b33a2
zeroknots <zeroknots@protonmail.com>
2024-01-15 11:35:11 +0700

    chore: created interface for session key manger

---
+++ src/ISessionKeyManager.sol
@@ -0,0 +1,49 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.4;
+
+interface Interface {
+    type ValidationData is uint256;
+
+    struct PackedUserOperation {
+        address sender;
+        uint256 nonce;
+        bytes initCode;
+        bytes callData;
+        bytes32 accountGasLimits;
+        uint256 preVerificationGas;
+        uint256 maxFeePerGas;
+        uint256 maxPriorityFeePerGas;
+        bytes paymasterAndData;
+        bytes signature;
+    }
+
+    struct SessionData {
+        uint48 validUntil;
+        uint48 validAfter;
+        address sessionValidationModule;
+        bytes sessionKeyData;
+    }
+
+    event SessionCreated(address indexed sa, bytes32 indexed sessionDataDigest, SessionData data);
+    event SessionDisabled(address indexed sa, bytes32 indexed sessionDataDigest);
+
+    function digest(SessionData memory sessionData) external pure returns (bytes32);
+    function disableSession(bytes32 _sessionDigest) external;
+    function enableSession(SessionData memory sessionData) external;
+    function getSessionData(address smartAccount, bytes32 sessionDigest)
+        external
+        view
+        returns (SessionData memory data);
+    function isModuleType(uint256 _type) external pure returns (bool);
+    function isValidSignatureWithSender(address sender, bytes32 hash, bytes memory data)
+        external
+        view
+        returns (bytes4);
+    function name() external pure returns (string memory);
+    function onInstall(bytes memory data) external;
+    function onUninstall(bytes memory data) external;
+    function validateUserOp(PackedUserOperation memory userOp, bytes32 userOpHash)
+        external
+        returns (ValidationData vd);
+    function version() external pure returns (string memory);
+}
